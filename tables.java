4b86e5364ca9a618bb6b506ab5fdf33821a8c08f
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-06-30 09:47:22 +0530

    This is the employee tables details

---
+++ tables.java
@@ -0,0 +1,7 @@
+This is the program to write daily working hours in a table
+
+Need to update every day without fail.
+
+All the rows should be in alphabatical order
+
+Please make sure everyone record should be updated. 
