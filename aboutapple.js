9ad38b03011194358edde284a3dc19e0408b6559
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-02 06:45:07 +0530

    About the Apple Company

---
+++ aboutapple.js
@@ -0,0 +1,7 @@
+Apple Inc., formerly Apple Computer, Inc., American manufacturer of personal computers, smartphones, tablet computers, computer peripherals, and computer software. It was the first successful personal computer company and the popularizer of the graphical user interface. Headquarters are located in Cupertino, California.
+
+
+	Garage start-up
+Apple Inc. had its genesis in the lifelong dream of Stephen G. Wozniak to build his own computer—a dream that was made suddenly feasible with the arrival in 1975 of the first commercially successful microcomputer, the Altair 8800, which came as a kit and used the recently invented microprocessor chip. Encouraged by his friends at the Homebrew Computer Club, a San Francisco Bay area group centred around the Altair, Wozniak quickly came up with a plan for his own microcomputer. In 1976, when the Hewlett-Packard Company, where Wozniak was an engineering intern, expressed no interest in his design, Wozniak, then 26 years old, together with a former high-school classmate, 21-year-old Steve Jobs, moved production operations to the Jobs family garage.
+
+
---
+++ devops_role.py
@@ -0,0 +1,6 @@
+Roles and Responsibilities in Github for Devops engineer
+
+1 Create a Repository 
+2 Create a team
+3 Add users to the team
+4 Provide the repository access to the team
