1364a9002c4c32fe64a9ef3138dae7feca8f486c
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-04 05:38:52 +0530

    This is for Applelatops 2021 Release dates

---
+++ 2021_Macs.py
@@ -0,0 +1,5 @@
+Apple launched its long-rumoured M1 based Macbooks back in November last year.
+
+This marked an end to the 15-year-old alliance between intel and Apple that began in 2005. The Macbook Air that was refreshed in 2020 itself was also upgraded with the M1 chip. 
+
+Apple claimed major improvements in performance and battery life on the Macbook M1 Air, which came out to be true. The new in-house chipset did wonders to this new Macbook Air.
