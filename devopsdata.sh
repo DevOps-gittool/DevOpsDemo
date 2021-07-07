eef30c338d922f4220a99191b65f0683a989ef67
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-07 15:36:27 +0000

    maven and tomcat details

---
+++ devopsdata.sh
@@ -0,0 +1,14 @@
+
+/opt/apache-maven-3.8.1
+
+export M2_HOME=/opt/apache-maven-3.8.1
+
+export PATH=$PATH:$M2_HOME/bin
+
+Tomcat download link: https://downloads.apache.org/tomcat/tomcat-9/v9.0.50/bin/apache-tomcat-9.0.50.zip
+
+http://35.154.147.178:8080
+
+new link
+
+http://35.154.147.178:8011
